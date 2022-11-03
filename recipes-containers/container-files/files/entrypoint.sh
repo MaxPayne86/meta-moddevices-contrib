b1d1448957739772f45a6ff60cc0ae6872643955
Massimo Pennazio <maxipenna@libero.it>
2022-11-03 11:34:50 +0100

    Removed unavailable variables

--- recipes-containers/container-files/files/entrypoint.sh
+++ recipes-containers/container-files/files/entrypoint.sh
@@ -36,18 +36,13 @@ start_modui()
     #export LV2_PATH=$LV2_PATH:/usr/lib/lv2:/home/root/.lv2plugins
     mkdir -p /home/root/.pedalboards
     mkdir -p /home/root/.data/mod-ui
-    export LV2_PLUGIN_DIR=/usr/lib/lv2
-    #export LV2_PEDALBOARDS_DIR=/home/root/.pedalboards/
     export MOD_DATA_DIR=/home/root/.data/mod-ui
     export MOD_DEV_ENVIRONMENT=0
-    export MOD_DEVICE_WEBSERVER_PORT=8080
     #export MOD_LOG=1
     export MOD_LOG=0
     export MOD_APP=0
     export MOD_DEV_HMI=0
     #export MOD_HMI_SERIAL_PORT="/dev/ttyUSB1"
-    export MIDI_CH_PEDALBOARDS_NAV=1
-    export MIDI_CH_SNAPSHOTS_NAV=2
     export MOD_USER_FILES_DIR=/home/root/user-files
     cd /usr/local/mod-ui
     /usr/bin/python3 -u server.py 2>&1|awk '{print "[modui]: "$0}' > /dev/stdout &
